5eca17084e2f90f1b15308c18686ce65d42dfa82
Phil Dunlea <pdunlea@gmail.com>
2023-05-09 09:40:26 -0400

    added missing network things

--- pihole/setup.sh
+++ pihole/setup.sh
@@ -1,11 +1,14 @@
 apt update
 apt upgrade -y
+apt install network-manager -y
+systemctl start NetworkManager.service
 
-cp 01-network-manager-all.yaml /etc/netplan
+cd ~
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+
+cp homelab/pihole/01-network-manager-all.yaml /etc/netplan
 cd /etc/netplan
 
 netplan try
-cd ~
-git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
 cd "Pi-hole/automated install/"
 sudo bash basic-install.sh
