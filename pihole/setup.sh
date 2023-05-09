57c91e942e227025c991601a3518d5e9ae1979d2
Phil Dunlea <pdunlea@gmail.com>
2023-05-09 09:32:01 -0400

    pihole install updated

--- pihole/setup.sh
+++ pihole/setup.sh
@@ -1,5 +1,11 @@
 apt update
 apt upgrade -y
+
+cp 01-network-manager-all.yaml /etc/netplan
+cd /etc/netplan
+
+netplan try
+cd ~
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
 cd "Pi-hole/automated install/"
 sudo bash basic-install.sh
