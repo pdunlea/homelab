83ad89f8a2c79df9696b00c4805dc9f0a9d8d1c1
Phil Dunlea <pdunlea@gmail.com>
2023-05-30 10:16:22 -0400

    cleanup setup files.

--- crontasks/setup3.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install cifs-utils -y
-sudo apt install nfs-common -y
-sudo mkdir /media/nas_share
-sudo echo '192.168.1.9:/mnt/MainStorage /media/nas_share nfs rw 0 0' >> /etc/fstab
-
-pyenv install 3.10
-pyenv global 3.10
-reboot
