64928b2fc104968eda360622bb60b2401c1a82d3
Phil Dunlea <pdunlea@gmail.com>
2023-05-30 10:14:48 -0400

    fix setup3 and move mount to the run command.

--- crontasks/run.sh
+++ crontasks/run.sh
@@ -1,5 +1,3 @@
-sudo mount -t nfs 192.168.1.9:/mnt/MainStorage /media/nas_share
-
-CONFIG=.config/gphotos-sync
+CONFIG=~/.config/gphotos-sync
 STORAGE=/media/nas_share/My_photos_backup
 docker run --rm -v $CONFIG:/config -v $STORAGE:/storage -it ghcr.io/gilesknap/gphotos-sync /storage
