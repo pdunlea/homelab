17310e9c054ec32cc3816a71bcb1cf20b63477f0
Phil Dunlea <pdunlea@gmail.com>
2023-05-30 10:10:04 -0400

    fix setup3 and move mount to the run command.

--- crontasks/setup3.sh
+++ crontasks/setup3.sh
@@ -1,7 +1,6 @@
 sudo apt install cifs-utils -y
 sudo apt install nfs-common -y
-yum install nfs-utils
-mount -t nfs 192.168.1.9:/mnt/MainStorage /media/nas_share
+sudo mkdir /media/nas_share
 
 pyenv install 3.10
 pyenv global 3.10
