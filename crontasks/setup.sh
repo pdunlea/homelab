4390c12969b0671151e089c08c2faa531ee93c5f
Phil Dunlea <pdunlea@gmail.com>
2023-06-03 11:23:09 -0400

    got rid of docker

--- crontasks/setup.sh
+++ crontasks/setup.sh
@@ -1,7 +1,9 @@
 sudo chmod +x setup2.sh
 sudo chmod +x setup3.sh
 sudo chmod +x run.sh
+sudo chmod +x get-pip.sh
 sudo apt update
 
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
+./setup2.sh
--- crontasks/setup3.sh
+++ crontasks/setup3.sh
@@ -6,3 +6,5 @@ cp /mnt/nas_share/client_secret.json ~/.config/gphotos-sync/client_secret.json
 cd ~
 cd homelab/crontasks
 
+python3 get-pip.py
+python3 -m pip install gphotos-sync
