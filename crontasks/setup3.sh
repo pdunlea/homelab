77e0aa0aca2c3f646db92ecaddffbab90688d4d3
Phil Dunlea <pdunlea@gmail.com>
2023-06-03 11:27:09 -0400

    fixed an oops

--- crontasks/setup3.sh
+++ crontasks/setup3.sh
@@ -1,7 +1,6 @@
 cd ~
 mkdir .config
 mkdir .config/gphotos-sync
-more homelab/crontasks/setup3.sh
 cp /mnt/nas_share/client_secret.json ~/.config/gphotos-sync/client_secret.json
 cd ~
 cd homelab/crontasks
